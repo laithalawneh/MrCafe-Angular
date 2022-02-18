5554153f68e04d6ddf8a672cb5b3e6cb4a21e356
laith <roioakll@gamil.com>
2022-02-18 05:15:18 +0200

    singup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { SignupserviceService } from '../service/signupservice.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  fname?:string
+  lname?:string
+  email?:string
+  phone?:string
+  latitude?:number
+  longitude?:number
+  salary?:number
+  userName?:string
+  password?:string
+  userId?:number
+  rolename?:string
+  CafeId?:number
+
+form1=new FormGroup({
+  fname:new FormControl('',[Validators.required]),
+  lname:new FormControl('',[Validators.required]),
+  email:new FormControl('',[Validators.required,Validators.email]),
+  phone:new FormControl('',[Validators.required]),
+  latitude:new FormControl('',[Validators.required]),
+  longitude:new FormControl('',[Validators.required]),
+  salary:new FormControl('',[Validators.required]),
+  userName:new FormControl('',[Validators.required]),
+  password:new FormControl('',[Validators.required]),
+  userId:new FormControl('',[Validators.required]),
+  rolename:new FormControl('',[Validators.required]),
+  CafeId:new FormControl('',[Validators.required])
+
+})
+  constructor(public services:SignupserviceService ,private toaster:ToastrService) { 
+
+   
+  }
 
   ngOnInit(): void {
+  }
+  Insertusers(){
+   let object={
+       
+  fname:this.fname,
+  lname:this.lname,
+  email:this.email,
+  phone:this.phone,
+  latitude:this.latitude,
+  longitude:this.longitude,
+  salary:this.salary,
+  userName:this.userName,
+  password:this.password,
+  userId:this.userId,
+  rolename:this.rolename,
+  CafeId:this.CafeId
+      }
+    console.log(this.form1.value)
+    this.services.insertUser(this.form1.value)
+    
+
+
   }
 
 }
